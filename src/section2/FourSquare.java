a28a0aa231242948f6f1f607dc564a98cf224530
League Student <league@iMac-14.attlocal.net>
2019-09-28 12:55:42 -0700

    Day 2 of Workshop
    made random shapes with loop

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -4,30 +4,35 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
-	// 2. Create a new Robot
+
+	Robot robot = new Robot();
 
 	void go() {
-		// 4. Make the robot move as fast as possible
+		robot.setSpeed(1000);
 
-		// 5. Set the pen width to 5
+		robot.setPenWidth(10);
 
-		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		for (int i = 0; i < 4; i++) {
+			robot.setRandomPenColor();
+			drawSquare();
+			robot.turn(90);
+		}
 
-			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
+		// 1. Call the drawSquare() method
+		
+
+		// 8. Turn the robot 90 degrees to the right
 
 	}
 
-	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		robot.penDown();
+		for (int i = 0; i < 4; i++) {
+			robot.move(100);
+			robot.turn(-90);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -35,6 +40,3 @@ public class FourSquare {
 	}
 
 }
-
-
-
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,35 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot robot = new Robot();
+	robot.sparkle();
+	robot.setSpeed(100);
+	robot.move(100);
+	robot.turn(-90);
+	robot.setPenColor(255,0,0);
+	robot.penDown();
+	for(int i=0; i<4; i++) {
+		robot.move(100);
+		robot.turn(-90);
+	}
+	robot.penUp();
+	robot.turn(90);
+	robot.move(-20);
+	robot.turn(-90);
+	robot.move(20);
+	robot.penDown();
+	robot.move(100);
+	for(int i=0; i<3; i++) {
+	robot.turn(-90);
+	robot.move(100);
+	}
+	robot.penUp();
+	robot.move(-100);
+	robot.turn(45);
+	robot.penDown();
+	robot.move(29);
+	robot.penUp();
 	
-		
 		
 	}
 }
